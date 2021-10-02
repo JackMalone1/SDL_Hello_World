4a344d03fb297140abdcecfa9a46a8a41a7dccc7
Jack Malone <c00236428@itcarlow.ie>
2021-10-02 13:49:16 +0100

    fixed issues with not loading

---
+++ src/game.cpp
@@ -0,0 +1,26 @@
+#include "../include/game.h"
+
+bool Game::isRunning()
+{
+    return true;
+}
+
+void Game::handleEvents()
+{
+
+}
+
+void Game::update()
+{
+
+}
+
+void Game::render()
+{
+
+}
+
+void Game::cleanup()
+{
+
+}
